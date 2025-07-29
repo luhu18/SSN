e07d17dfae4f44f204dd73987a0faaecc9bc86d9
Lu Hu <husseinlucien7@gmail.com>
2025-07-29 11:40:17 +0300

    Minor updates on the contact page.

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'; // Import useState
 import PageHeader from '../components/PageHeader';
+import { RiMastodonFill } from 'react-icons/ri';
 
 const ContactPage = () => {
   // State for form fields
@@ -110,7 +111,7 @@ const ContactPage = () => {
                     <div>
                       <h4 className="font-medium">Address</h4>
                       {/* Updated address based on the current location context */}
-                      <p className="text-gray-600">Kakuma, Turkana County, Kenya<br />Kampala, Uganda</p>
+                      <p className="text-gray-600">Kiboga District, Lwamata Town Council, Lunnya Village, Uganda</p>
                     </div>
                   </div>
 
@@ -121,7 +122,7 @@ const ContactPage = () => {
                     <div>
                       <h4 className="font-medium">Phone</h4>
                       {/* Updated phone numbers based on Footer */}
-                      <p className="text-gray-600">Kenya: +254 7XX XXX XXX</p>
+                      <p className="text-gray-600">Uganda: +256 779763525</p>
                       <p className="text-gray-600">Uganda: +256 783 605958</p>
                     </div>
                   </div>
@@ -146,10 +147,16 @@ const ContactPage = () => {
                       <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" />
                     </svg>
                   </a>
-                  <a href="https://twitter.com/yoursustainableschoolsnetwork" target="_blank" rel="noopener noreferrer" className="bg-green-100 text-green-600 p-3 rounded-full hover:bg-green-200 transition duration-300" aria-label="Twitter">
-                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                    </svg>
+                   {/* mastodon*/}
+                               <a
+                    href="https://mastodon.social/@ehsenvironmentalclub1" 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Mastodon"
+                    className="bg-green-100 text-green-600 p-3 rounded-full hover:bg-green-200 transition duration-300"
+                   >
+                    {/* Use the FaMastodon component directly */}
+                    <RiMastodonFill className="w-6 h-6" />
                   </a>
                   <a href="https://instagram.com/yoursustainableschoolsnetwork" target="_blank" rel="noopener noreferrer" className="bg-green-100 text-green-600 p-3 rounded-full hover:bg-green-200 transition duration-300" aria-label="Instagram">
                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
